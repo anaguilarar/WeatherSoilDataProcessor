3d473490f035a4b0c2572039dd4d652a014020ed
Aguilar Ariza <Andres.Aguilar@cgiar.org>
2025-03-26 16:09:47 -0500

    dssat run simulation

--- crop_modeling/dssat/base.py
+++ crop_modeling/dssat/base.py
@@ -1,32 +1,31 @@
-
+import concurrent.futures
+import glob
 import os
+import platform
+import shutil
+import subprocess
+from pathlib import Path
+from typing import List, Optional
+
+import pandas as pd
+from omegaconf import OmegaConf
+from tqdm import tqdm
+
 from DSSATTools.base.sections import Section, clean_comments
 from DSSATTools.crop import CROPS_MODULES, VERSION, Crop
 from DSSATTools.run import CONFILE
 
-import pandas as pd
-from typing import Optional, List
-from tqdm import tqdm
-from pathlib import Path
-
-from .files_export import from_soil_to_dssat,from_weather_to_dssat
+from ._base import DSSATFiles, coords_from_soil_file, section_indices
+from .files_export import from_soil_to_dssat, from_weather_to_dssat
+from .files_reading import (delimitate_header_indices,
+                           join_row_using_header_indices)
 from .management import DSSATManagement_base
-import glob
-from .files_reading import delimitate_header_indices, join_row_using_header_indices
-from omegaconf import OmegaConf
-import subprocess
-from ._base import DSSATFiles, section_indices, coords_from_soil_file
-#from multiprocessing import Pool
-import concurrent.futures
-import shutil
-import platform
 from ..utils.model_base import ModelBase
 
-
-def check_soil_id(management_pathfile, new_soil_id):
+def check_soil_id(management_pathfile:str, new_soil_id:str):
     
 
-    lines = DSSATFiles.open_file(management_pathfile[0])
+    lines = DSSATFiles.open_file(management_pathfile)
     section_id = list(section_indices(lines, pattern= '*FIELDS'))[0]+1
     section_header_str = lines[section_id]
     header_indices = delimitate_header_indices(section_header_str)
@@ -46,7 +45,7 @@ def check_soil_id(management_pathfile, new_soil_id):
 
         lines[section_id+1] = join_row_using_header_indices(section_header_str, section_data_str, row_to_replace = datalineinfo.values[0])
     
-        with open(management_pathfile[0], 'w') as file:
+        with open(management_pathfile, 'w') as file:
             for line in lines:
                 file.write(f"{line}")
                 
@@ -93,61 +92,49 @@ def check_exp_summary_name(workdir_path, run_path, experiment_id, removeworking_
         valtoreturn = {os.path.basename(run_path): False}
         
     return valtoreturn
-    
-def run_experiment_dssat(path, experimentid,crop_code, crop,bin_path = None, dssat_path = None, remove_folder = False):
 
+def run_dssat_simulation(experiment_id, path, crop_code, crop, dssat_home, dssat_bin_path, crd_path, sl_path, std_path, sim_experiment_path):
+    
     exp_pathfile = glob.glob(path+'/*.{}X*'.format(crop_code))
+    
     if len(exp_pathfile)==0:
         print(' There is no experimental file, please generated first')
         return {os.path.basename(path): False}
     if len(exp_pathfile)>1:
         print(' There are more than one experimental file, please only leave one')
         return {os.path.basename(path): False}
-        
+    
     exp_pathfile = os.path.basename(exp_pathfile[0])
-    #create_DSSBatch(exp_pathfile[0])
-    dssatpath = os.path.dirname(bin_path) if dssat_path is None else dssat_path
-    tmppath = os.path.join(path, f'_{experimentid}')
-    create_dssat_tmp_env(path, tmppath, exp_pathfile)
+    dssat_home = os.path.dirname(dssat_bin_path) if dssat_home is None else dssat_home
+    wth_path = os.path.join(sim_experiment_path,'WTHE0001')
+    create_dssat_tmp_env(path, sim_experiment_path, exp_pathfile)
+    create_dssat_config_path_file(sim_experiment_path, crop_code, crop, dssat_home, wth_path, crd_path, sl_path, std_path)
     
-    create_dssat_config_path_file(tmppath, crop_code,crop, dssatpath,os.path.join(tmppath,'WTHE0001') , 
-                                os.path.join(dssat_path, 'Genotype'), os.path.join(dssat_path, 'Soil'), os.path.join(dssat_path, 'StandardData'))
-        
-    subprocess.run([bin_path, 'C', exp_pathfile, str(experimentid)],
-                    cwd=tmppath, capture_output=True, text=True,
-                    env = {"DSSAT_HOME":dssatpath})
+    subprocess.run([dssat_bin_path, 'C', exp_pathfile, str(experiment_id)],
+                    cwd=sim_experiment_path, capture_output=True, text=True,
+                    env={"DSSAT_HOME": dssat_home, })
+
+
+def run_experiment_dssat(path, experimentid,crop_code, crop,bin_path = None, dssat_path = None, remove_folder = False, sim_experiment_path = None):
+    sim_experiment_path = sim_experiment_path or os.path.join(path, f'_{experimentid}')
     
-    proccess_finsihed = check_exp_summary_name(tmppath, path, experimentid, removeworking_path_folder=remove_folder)
+    run_dssat_simulation(experimentid, path, crop_code, crop, dssat_path, bin_path, os.path.join(dssat_path, 'Genotype'),
+            os.path.join(dssat_path, 'Soil'),os.path.join(dssat_path, 'StandardData'),sim_experiment_path)
+    
+    proccess_finsihed = check_exp_summary_name(sim_experiment_path, path, experimentid, removeworking_path_folder=remove_folder)
     
     return proccess_finsihed
 
 
-def run_experiment_dssat_bin(path, experimentid,crop_code, crop, remove_folder = False):
+def run_experiment_dssat_bin(path, experimentid,crop_code, crop, remove_folder = False, sim_experiment_path = None):
     from DSSATTools.run import CRD_PATH, SLD_PATH, DSSAT_HOME, BIN_PATH, STD_PATH
     
+    sim_experiment_path = sim_experiment_path or os.path.join(path, f'_{experimentid}')
     
-    exp_pathfile = glob.glob(path+'/*.{}X*'.format(crop_code))
-    if len(exp_pathfile)==0:
-        print(' There is no experimental file, please generated first')
-        return {os.path.basename(path): False}
-    if len(exp_pathfile)>1:
-        print(' There are more than one experimental file, please only leave one')
-        return {os.path.basename(path): False}
-        
-    exp_pathfile = os.path.basename(exp_pathfile[0])
-    #create_DSSBatch(exp_pathfile[0])
+    run_dssat_simulation(experimentid, path, crop_code, crop, DSSAT_HOME, BIN_PATH, CRD_PATH,
+            SLD_PATH,STD_PATH,sim_experiment_path)
     
-    tmppath = os.path.join(path, f'_{experimentid}')
-    create_dssat_tmp_env(path, tmppath, exp_pathfile)
-
-    wth_path = os.path.join(tmppath,'WTHE0001')
-    create_dssat_config_path_file(tmppath, crop_code, crop, DSSAT_HOME, wth_path, CRD_PATH, SLD_PATH, STD_PATH)
-        
-    subprocess.run([BIN_PATH, 'C', exp_pathfile, str(experimentid)],
-                    cwd=tmppath, capture_output=True, text=True,
-                    env={"DSSAT_HOME": DSSAT_HOME, })
-    
-    proccess_finsihed = check_exp_summary_name(tmppath, path, experimentid, removeworking_path_folder=remove_folder)
+    proccess_finsihed = check_exp_summary_name(sim_experiment_path, path, experimentid, removeworking_path_folder=remove_folder)
     
     return proccess_finsihed
 
@@ -171,9 +158,8 @@ class DSSATBase(ModelBase):
         template: Optional[str] = None,
         roi_id: int = 1,
         plantingWindow: Optional[int] = None,
-        fertilizer: Optional[bool] = None,
         index_soilwat: int = 1,
-        use_r = False
+        fertilizer_schedule = None
         ) -> None:
         """
         Set up the management configuration and create DSSAT experiment files.
@@ -207,20 +193,14 @@ class DSSATBase(ModelBase):
                                 n_planting_windows = plantingWindow)
         
         for pathiprocess in self._process_paths:
-            if use_r:
-                
-                output = dssatm.create_file_using_rdssat(template, pathiprocess, roi_id = roi_id, plantingWindow = plantingWindow, 
-                                            fertilizer = fertilizer, index_soilwat = index_soilwat)
-            else:
-                
-                soil = glob.glob(pathiprocess+'/*.SOL*')
-                lat, long = coords_from_soil_file(soil[0])
-                
-                output = dssatm.create_file(template, pathiprocess, roi_id = roi_id,plantingWindow = plantingWindow,
-                        fertilizer = fertilizer, index_soilwat = index_soilwat,
-                        long = long, lat = lat)
-                self.crop_code = dssatm.crop_code
 
+            soil = glob.glob(pathiprocess+'/*.SOL*')
+            lat, long = coords_from_soil_file(soil[0])
+            
+            output = dssatm.create_file(template, pathiprocess, roi_id = roi_id,plantingWindow = plantingWindow,
+                    index_soilwat = index_soilwat, fertilizer_schedule =fertilizer_schedule,
+                    long = long, lat = lat)
+            self.crop_code = dssatm.crop_code
             if output is None:
                 continue
             experiment_config = OmegaConf.load(os.path.join(pathiprocess, 'experimental_file_config.yaml'))
@@ -228,7 +208,7 @@ class DSSATBase(ModelBase):
             management_pathfile = glob.glob(pathiprocess+'/*.{}*'.format(experiment_config.MANAGEMENT.crop_code))
             print(f'experimental file created: {management_pathfile}')
             
-            check_soil_id(management_pathfile, experiment_config.SOIL.ID_SOIL )
+            check_soil_id(management_pathfile[0], experiment_config.SOIL.ID_SOIL )
             
     
     def set_up_crop(self, crop: str, cultivar: str, cultivar_template: str = None) -> None:
@@ -251,7 +231,7 @@ class DSSATBase(ModelBase):
         for pathtiprocess in self._process_paths:
             crop_manager.write(pathtiprocess)
     
-    def run(self, crop_code, crop, planting_window, bin_path = None, parallel_tr= True, ncores = 10, remove_tmp_folder = False, dssat_path:str = None) -> None:
+    def run(self, crop_code, crop, planting_window, bin_path = None, parallel_tr= True, ncores = 10, remove_tmp_folder = False, dssat_path:str = None, sim_experiment_path = None) -> None:
         
         """
         Run DSSAT simulations for all processing paths.
@@ -291,18 +271,18 @@ class DSSATBase(ModelBase):
                 with concurrent.futures.ThreadPoolExecutor(max_workers=ncores) as executor:
                         if bin_path is None:
                             future_to_tr ={executor.submit(run_experiment_dssat_bin, pathiprocess, i, 
-                                                        crop_code,crop, remove_folder = remove_tmp_folder): (i) for i in range(1,planting_window+1)}
+                                                        crop_code,crop, remove_folder = remove_tmp_folder, sim_experiment_path = sim_experiment_path): (i) for i in range(1,planting_window+1)}
                         else:
                             future_to_tr ={executor.submit(run_experiment_dssat, pathiprocess, i, 
-                                                        crop_code,crop, bin_path, dssat_path, remove_folder = remove_tmp_folder): (i) for i in range(1,planting_window+1)}
+                                                        crop_code,crop, bin_path, dssat_path, remove_folder = remove_tmp_folder, sim_experiment_path = sim_experiment_path): (i) for i in range(1,planting_window+1)}
 
                         for future in concurrent.futures.as_completed(future_to_tr):
                                 tr = future_to_tr[future]
                                 try:
-                                        file_path_pertr[str(tr)] = future.result()
+                                    file_path_pertr[str(tr)] = future.result()
                                         
                                 except Exception as exc:
-                                        print(f"Request for treatment {tr} generated an exception: {exc}")
+                                    print(f"Request for treatment {tr} generated an exception: {exc}")
                                         
                 process_completed[os.path.basename(pathiprocess)] = any([v[list(v.keys())[0]] for k,v in file_path_pertr.items()])
             
@@ -318,10 +298,10 @@ class DSSATBase(ModelBase):
                 for tr in range(1,planting_window+1):
                     if bin_path is None:
                         file_path_pertr[str(tr)] = run_experiment_dssat_bin(pathiprocess, tr, 
-                                                crop_code,crop, remove_folder = remove_tmp_folder)
+                                                crop_code,crop, remove_folder = remove_tmp_folder, sim_experiment_path = sim_experiment_path)
                     else:
                         file_path_pertr[str(tr)] = run_experiment_dssat(pathiprocess, tr, 
-                                                            crop_code,bin_path, remove_folder = remove_tmp_folder)
+                                                            crop_code,bin_path, remove_folder = remove_tmp_folder, sim_experiment_path = sim_experiment_path)
             
                 process_completed[os.path.basename(pathiprocess)] = any([v[list(v.keys())[0]] for k,v in file_path_pertr.items()])
 
@@ -411,5 +391,3 @@ class DSSATCrop_base(Crop):
                 name="ecotype", file_lines=file_lines, crop_name=self.crop_name,
                 code=self.cultivar["ECO#"]
             )
-
-                
